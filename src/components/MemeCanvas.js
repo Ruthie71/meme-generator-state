b22d6578c8872b82daa84dc7a7440a67f0edf950
Ruthie <ruthmckinney1971@gmail.com>
2021-12-01 14:23:06 +0100

    layout update

--- src/components/MemeCanvas.js
+++ src/components/MemeCanvas.js
@@ -2,7 +2,13 @@ import { forwardRef } from "react";
 
 const MemeCanvas = forwardRef(({ selectedTemplate: { url, name } }, ref) => {
     return (
-        <div className="col-md-4" style={{ marginTop: "10px" }}>
+        <div
+            className="col-md-4"
+            xs={2}
+            md={4}
+            lg={6}
+            style={{ marginTop: "10px" }}
+        >
             <h1>MEME GENERATOR</h1>
             <div>Step 1: Pick your meme</div>
             <div>Step 2: Write your text in the boxes provided</div>
